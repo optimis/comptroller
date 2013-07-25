ce149bb15f0c055b1853e382b640ca858c2901ea
hubert <hubert77@gmail.com>
2013-07-25 11:43:57 -0700

    Allow specificiation of url of api when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'support/mimic'
 require 'comptroller'
 
+her_url = ENV['API_URL'] || 'http://127.0.0.1:11988'
+
 BILLING_API = Her::API.new
-BILLING_API.setup url: 'http://127.0.0.1:11988' do |c|
+BILLING_API.setup url: her_url do |c|
   c.use Faraday::Request::UrlEncoded
   c.use Her::Middleware::DefaultParseJSON
   c.use Faraday::Adapter::NetHttp
