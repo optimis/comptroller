d3269f2fcc685de4f9c6270ae36b293c6654bd20
hubert <hubert77@gmail.com>
2013-08-05 13:10:18 -0700

    squash

--- spec/claim_error_spec.rb
+++ spec/claim_error_spec.rb
@@ -5,5 +5,16 @@ module Comptroller
     it 'retrieves claim errors' do
       expect(ClaimError.all.first).to eql(ClaimError.new(:id => 1))
     end
+
+    it 'allows deleting of claim errors' do
+      error = ClaimError.all.first
+      ClaimError.destroy_existing(error.id)
+      expect(ClaimError.all).to be_empty
+    end
+
+    it 'allows updating of errors' do
+      error = ClaimError.save_existing(1, :message_body => 'foo bar')
+      expect(ClaimError.find(1).message_body).to eql('foo bar')
+    end
   end
 end
