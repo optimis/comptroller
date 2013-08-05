497138dd31b75e2ea94b4554ec7fa7f472ebc8fb
hubert <hubert77@gmail.com>
2013-08-05 13:10:10 -0700

    Include root in json for claim errors

--- lib/comptroller/claim_error.rb
+++ lib/comptroller/claim_error.rb
@@ -3,6 +3,7 @@ module Comptroller
     include Her::Model
     use_api BILLING_API
     collection_path '/duxware_errors'
+    include_root_in_json true
 
     def eql?(other)
       id.eql? other.id
